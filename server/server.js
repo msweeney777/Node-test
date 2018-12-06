bac00ead1bc29df72763654b4e85c2f8170945f5
Matt Sweeney <msweeney777@gmail.com>
2018-12-06 15:01:32 -0500

    Added supertest; experimented further with assertions; server.test.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
 });
 
+// Get /users
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'J. Obediah "Obie" Danciewicz-Helmers',
+    age: 23
+  }, {
+    name: 'Matt Sweeney',
+    age: 24
+  }, {
+    name: 'Glen Bossie',
+    age: 23
+  }]);
+});
+// Give users a name property
+
 app.listen(3000);
+module.exports.app = app;
