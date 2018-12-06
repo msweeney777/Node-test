ca58beb87707c56ed6a900627e8c21d8ac0e9513
Matt Sweeney <msweeney777@gmail.com>
2018-12-06 12:34:13 -0500

    Async testing with assertions; added async square and addition testing.

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,10 +8,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4,3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('should square a number', () => {
   var res = utils.square(11);
 
   expect(res).toBe(121).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(3, (res) => {
+    expect(res).toBe(9).toBeA('number')
+    done();
+  });
 });
 
 //should verify first and last names are set
